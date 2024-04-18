20effa7bd23fe50965b3f07949f1bef79387f0dd
Sam Boyarsky <296934+xenic@users.noreply.github.com>
2024-04-18 10:07:19 -0400

    First pass CGOL (#5)
    * add swp files to gitignore
    
    * add first pass student selector
    
    * with replace child in list item
    
    * student selector and looping updates
    
    * first pass cgol

--- studentSelector/studentSelector.js
+++ studentSelector/studentSelector.js
@@ -6,6 +6,67 @@ function pick() {
     // display the number
     let rnd = Math.floor(Math.random() * students.length);
     let selectedStudent = students[rnd];
-    console.log(document.getElementsByTagName("p").length);
-    document.getElementsByTagName("p")[0].textContent = selectedStudent;
+    // console.log(document.getElementsByTagName("p").length);
+    // document.getElementsByTagName("p")[0].textContent = selectedStudent;
+    // document.getElementById("selectedStudentPlace").textContent += selectedStudent;
+    let studentList = document.getElementById("selectedStudentPlace");
+    let placeholder = studentList.firstElementChild;
+    console.log(placeholder.textContent);
+    let newItem = document.createElement("li");
+    newItem.textContent = selectedStudent;
+    if (placeholder.textContent in students) {
+        studentList.appendChild(newItem);
+    } else {
+        studentList.replaceChild(newItem, placeholder);        
+    }
+    
+}
+
+function initialize() {
+    console.log("initialize");
+}
+
+function condition() {
+    console.log("condition");
+    return Math.random() > .5;
+}
+
+function update() {
+    console.log("update");
+}
+
+function iterate(){
+    // for(initialize(); condition(); update()) {
+    //     console.log("something happened");
+    // }
+
+    // for(let i = 0; i < students.length; i++) {
+    //     console.log(students[i]);
+    // }
+
+    // for(x in collection) iterates over the indices of the collection.
+    // for(student in students) {
+    //     console.log(students[student]);
+    // }
+
+    // for(x of collection) iterates over the elements of the collection.
+    // for(student of students) {
+    //     console.log(student);
+    // }
+    // let x = prompt("What is the name of the mascot?");
+    // while(x !== "trogdor") {
+    //     console.log(x);
+    //     x = prompt("What is the name of the mascot?");
+    // }
+
+    let target = Number(prompt("enter a number"))
+    if(target < 0) {
+        for(let i = 0; i >= target; i--){
+            console.log(i);
+        }
+    } else {
+        for(let i = 0; i <= target; i++){
+            console.log(i);
+        }    
+    }
 }
